c68a2db1f2ba3629d56642b429c46a64d308e4ef
Tomikland <tomikland@gmail.com>
2018-02-09 22:59:34 +0100

    Add files via upload

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,26 +8,44 @@ public class PlayerMovement : MonoBehaviour {
     public float moveSpeed = 3f;
     public float turnSpeed = 1f;
     public float maxSpeed = 20f;
-    public Vector3 vel;
-    //public Vector3 movement;
+    public Vector3 startPosition;
+    public float maxTurn = 50f;
+    private float fixedRotation;
     Rigidbody rb;
+
+    void Clamp(float val , float min , float max)
+    {
+        if (val < min)
+            val = min;
+        else if (val >= max)
+            val = max;
+    }
+
     void Start () {
         rb = GetComponent<Rigidbody>();
+        startPosition = transform.position;
+        fixedRotation = rb.rotation.eulerAngles.y;
     }
 	
 	// Update is called once per frame
 	void Update () {
-
-	}
-    void FixedUpdate()
-    {
-        transform.Rotate(transform.up * turnSpeed * Input.GetAxis("Horizontal") * Time.fixedDeltaTime);
-
-        
+        float rotY = rb.rotation.eulerAngles.y;
+        if (rotY >= fixedRotation - maxTurn && rotY <= fixedRotation + maxTurn)
+            transform.Rotate(transform.up * turnSpeed * Input.GetAxis("Horizontal") * Time.deltaTime);
+        else if (rotY < fixedRotation - maxTurn)
+            transform.rotation = Quaternion.Euler(0, fixedRotation - maxTurn, 0);
+        else if (rotY > fixedRotation + maxTurn)
+            transform.rotation = Quaternion.Euler(0, fixedRotation + maxTurn, 0);
 
         if (rb.velocity.magnitude <= maxSpeed)
-        {
             rb.AddForce(transform.forward * moveSpeed);
+
+        //Just for development
+        if (rb.position.y < -2)
+        {
+            transform.position = startPosition;
+            rb.velocity = new Vector3(0 , 0 , 0);
+            rb.rotation = Quaternion.Euler(0f , -90f , 0f);
         }
-    }
+	}
 }
