291474c10b469ae4a15967f8bafa0db3762bdfb8
Tomikland <tomikland@gmail.com>
2018-03-13 22:43:11 +0100

    Add files via upload

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -10,18 +10,25 @@ public class PlayerMovement : MonoBehaviour {
     public float turnSpeed = 1f;
     public float jumpForce = 1f;
     public float maxTurn = 50f;
+    public float maxMultiplier = 0.1f;
+    public float moveMultiplier = 0.1f;
     public bool gameOn = true;
     private Vector3 startPos;
     private Rigidbody rb;
+
+    public Vector3 CoM = Vector3.zero;
     private Quaternion startRot;
     private GameManager gm;
     int i = 0;
 
     void Start () {
+
         startPos = transform.position;
         startRot = transform.rotation;
         rb = GetComponent<Rigidbody>();
         gm = GetComponent<GameManager>();
+
+        rb.centerOfMass = CoM;
     }
 
     void FixedUpdate()
@@ -32,14 +39,18 @@ public class PlayerMovement : MonoBehaviour {
             Physics.Raycast(rb.position, Vector3.down, out hit);
             if (transform.position.x <= -18)
             {
-                if (Input.GetKeyDown(KeyCode.Space) && hit.distance < 0.01)
+                if (Input.GetKeyDown(KeyCode.Space) && hit.distance < 0.05f)
                     rb.AddRelativeForce(Vector3.up * jumpForce);
                 transform.Rotate(0, Input.GetAxis("Horizontal") * turnSpeed * Time.fixedDeltaTime, 0);
             }
+            maxSpeed += Mathf.Abs(Time.fixedDeltaTime) * maxMultiplier;
+            moveSpeed += Mathf.Abs(Time.fixedDeltaTime) * moveMultiplier;
             if (rb.velocity.magnitude < maxSpeed)
                 rb.AddRelativeForce(Vector3.forward * moveSpeed * Time.fixedDeltaTime);
         }
     }
+
+
     public void ResetPlayer()
     {
         transform.position = startPos;
